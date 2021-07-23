4fb9dc2e1e75b0a8206536d424a79e902240b007
definev <devdive2406@gmail.com>
2021-07-23 23:57:37 +0700

    Correct implement brightness filter

--- lib/src/_internal/image_processor/image_processor.dart
+++ lib/src/_internal/image_processor/image_processor.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class ImageProcessor {
-  static ColorFilter brightness(int value) => ColorFilter.mode(
-        value > 0 ? Colors.white.withOpacity(value / 100) : Colors.black.withOpacity(-value / 150),
-        value > 0 ? BlendMode.softLight : BlendMode.colorBurn,
-      );
+  static ColorFilter brightness(int value) {
+    var m = List<double>.from(defaultColorMatrix);
+    m[4] = value / 100 * 150;
+    m[9] = value / 100 * 150;
+    m[14] = value / 100 * 150;
+
+    return ColorFilter.matrix(m);
+  }
 
   static ColorFilter contrast(int value) {
     final contrast = value / 100;
@@ -19,23 +23,25 @@ class ImageProcessor {
   }
 
   static ColorFilter saturation(int value) {
-    final saturation = value / 100;
+    // if (value == 0) return const ColorFilter.matrix(defaultColorMatrix);
+    final sat = (value + 100) / 100;
 
-    final m = List<double>.from(defaultColorMatrix);
-    final invSat = 1 - saturation;
-    final R = 0.3086 * invSat;
-    final G = 0.6094 * invSat;
-    final B = 0.0820 * invSat;
+    List<double> m = List.from(defaultColorMatrix);
 
-    m[0] = R + saturation;
+    double invSat = 1 - sat; //<---------invSat will be negative if sat bigger than 1
+    double R = 0.213 * invSat;
+    double G = 0.715 * invSat;
+    double B = 0.072 * invSat;
+
+    m[0] = R + sat;
     m[1] = G;
     m[2] = B;
     m[5] = R;
-    m[6] = G + saturation;
+    m[6] = G + sat;
     m[7] = B;
     m[10] = R;
     m[11] = G;
-    m[12] = B + saturation;
+    m[12] = B + sat;
 
     return ColorFilter.matrix(m);
   }
